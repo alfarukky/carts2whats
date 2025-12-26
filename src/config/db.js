5c18e56a4d3dd85fd6ff821670326155ca30400c
alfarukky <alfarukky@gmail.com>
2025-12-26 22:41:55 +0100

    chnages to DB config for prod deployment

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,10 @@
-import mysql from 'mysql2/promise';
-import 'dotenv/config';
+import mysql from "mysql2/promise";
+import "dotenv/config";
+
 // CREATE A SINGLE POOL — reused across the whole app
 export const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "",
-  database: process.env.DB_NAME || "carts2whats",
+  uri: process.env.MYSQL_URL,
   waitForConnections: true,   // queue when all connections are busy
   connectionLimit: 10,        // max 10 connections
   queueLimit: 0               // unlimited queued requests
 });
-
