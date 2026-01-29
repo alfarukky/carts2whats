6a93444859e3791da3eeecf00061ee15e5dc92af
alfarukky <alfarukky@gmail.com>
2026-01-29 13:45:21 +0100

    security: crsf implmented on the product logic

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,7 +1,7 @@
-import { Router } from "express";
-import { isLoggedIn } from "../middleware/auth.middleware.js";
-import { uploadSingle } from "../middleware/upload.middleware.js";
-import { validateCSRF } from "../middleware/csrf.middleware.js";
+import { Router } from 'express';
+import { isLoggedIn } from '../middleware/auth.middleware.js';
+import { uploadSingle } from '../middleware/upload.middleware.js';
+import { generateCSRF, validateCSRF } from '../middleware/csrf.middleware.js';
 
 import {
   listProducts,
@@ -12,28 +12,45 @@ import {
   deleteProduct,
   togglePopular,
   toggleStock,
-} from "../controllers/product.controllers.js";
+} from '../controllers/product.controllers.js';
 
 const productRoute = Router();
 
 // PUBLIC: show all general products
-productRoute.get("/", listProducts);
+productRoute.get('/', generateCSRF, listProducts);
 
 // ADMIN: add new product
-productRoute.get("/add", isLoggedIn, showAddProductForm);
-productRoute.post("/add", isLoggedIn, validateCSRF, uploadSingle("image"), addProduct);
+productRoute.get('/add', isLoggedIn, generateCSRF, showAddProductForm);
+productRoute.post(
+  '/add',
+  isLoggedIn,
+  uploadSingle('image'),
+  validateCSRF,
+  addProduct,
+);
 
 // ADMIN: edit product
-productRoute.get("/:id/edit", isLoggedIn, showEditProductForm);
-productRoute.post("/:id/edit", isLoggedIn, validateCSRF, uploadSingle("image"), updateProduct);
+productRoute.get('/:id/edit', isLoggedIn, generateCSRF, showEditProductForm);
+productRoute.post(
+  '/:id/edit',
+  isLoggedIn,
+  uploadSingle('image'),
+  validateCSRF,
+  updateProduct,
+);
 
 // ADMIN: delete product (FIXED: POST instead of GET)
-productRoute.post("/:id/delete", isLoggedIn, validateCSRF, deleteProduct);
+productRoute.post('/:id/delete', isLoggedIn, validateCSRF, deleteProduct);
 
 // ADMIN: toggle popular (FIXED: POST instead of GET)
-productRoute.post("/:id/toggle-popular", isLoggedIn, validateCSRF, togglePopular);
+productRoute.post(
+  '/:id/toggle-popular',
+  isLoggedIn,
+  validateCSRF,
+  togglePopular,
+);
 
 // ADMIN: toggle stock status (FIXED: POST instead of GET)
-productRoute.post("/:id/toggle-stock", isLoggedIn, validateCSRF, toggleStock);
+productRoute.post('/:id/toggle-stock', isLoggedIn, validateCSRF, toggleStock);
 
 export default productRoute;
